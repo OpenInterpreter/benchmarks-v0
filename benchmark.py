c04bfad61f46595fa8c989fd6ac4d19dae70c0b7
Koissi Adjorlolo <yotcho2@gmail.com>
2024-05-22 20:41:12 -0700

    Removed duplicate function

--- benchmark.py
+++ benchmark.py
@@ -47,18 +47,6 @@ class OpenInterpreterCommand(TypedDict):
     custom_instructions: NotRequired[str]
 
 
-def command_to_interpreter(cmd: OpenInterpreterCommand) -> OpenInterpreter:
-    interpreter = OpenInterpreter(import_computer_api=True)
-    interpreter.llm.model = cmd.get("model", interpreter.llm.model)  # type: ignore
-    interpreter.llm.context_window = cmd.get("context_window", interpreter.llm.context_window)  # type: ignore
-    interpreter.llm.api_base = cmd.get("api_base", interpreter.llm.api_base)  # type: ignore
-    interpreter.llm.api_key = cmd.get("api_key", interpreter.llm.api_key)  # type: ignore
-    interpreter.auto_run = cmd.get("auto_run", interpreter.auto_run)  # type: ignore
-    interpreter.os = cmd.get("os_mode", interpreter.os)  # type: ignore
-    interpreter.custom_instructions = cmd.get("custom_instructions", interpreter.custom_instructions)  # type: ignore
-    return interpreter
-
-
 class TaskResult(TypedDict):
     task_id: str
     command: OpenInterpreterCommand
