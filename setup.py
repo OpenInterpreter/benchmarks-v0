e25f720cd09cd8268fb4e471d770849aa0760659
Koissi Adjorlolo <yotcho2@gmail.com>
2024-05-24 17:20:20 -0700

    Described, refactored setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ import subprocess
 from pathlib import Path
 
 
+"""
+This script is responsible for downloading benchmarks.  It's imperative that the files this script
+downloads are NOT committed to this repository.
+"""
+
+
 color = lambda c: lambda s: f"\\033[{c}m{s}\\033[0m"
 red = color(91)
 green = color(92)
@@ -14,9 +20,10 @@ def run_cmd(cmd: str):
 print(green("logging into huggingface"))
 run_cmd("huggingface-cli login")
 
-if not Path(".datasets"):
+datasets = Path(".datasets")
+if not datasets.exists():
     print(green("creating .datasets directory"))
-    run_cmd("mkdir -p .datasets")
+    datasets.mkdir(parents=True)
 
 if not Path(".datasets/GAIA").exists():
     print(green("downloading gaia"))
