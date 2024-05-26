cffedff112efaf692b823aad6cd9b12e8dba07d8
Koissi Adjorlolo <yotcho2@gmail.com>
2024-05-25 18:03:24 -0700

    Local saves (#3)
    * datasets and results now in .local dir

---
+++ constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+LOCAL = Path(".local")
+DATASETS = LOCAL / Path("datasets")
+GAIA = DATASETS / Path("GAIA")
--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-import benchmark
-
-runner = benchmark.DockerBenchmarkRunner()
-messages = runner.run(lambda _: None, {"auto_run": True, "api_key": os.environ.get("OPENAI_API_KEY", "")}, "sleep for 2 seconds", True)
-print(messages)
